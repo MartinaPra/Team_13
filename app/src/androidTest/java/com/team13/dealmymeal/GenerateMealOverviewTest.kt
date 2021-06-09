5c80b0bcd6ddbf437980511e364c23764c6bc382
nataliekvita <79967052+nataliekvita@users.noreply.github.com>
2021-06-09 09:33:11 +0200

    DMM-010 create history button and new test [KN,PM]

--- app/src/androidTest/java/com/team13/dealmymeal/GenerateMealOverviewTest.kt
+++ app/src/androidTest/java/com/team13/dealmymeal/GenerateMealOverviewTest.kt
@@ -1,17 +1,33 @@
 package com.team13.dealmymeal
 
-import android.content.Context
-import androidx.room.Room
-import com.team13.dealmymeal.data.DBManager
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class GenerateMealOverviewTest {
 
+
+    @get:Rule
+    val activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
     @Before
+    @Test
     fun setUp() {
         onView(ViewMatchers.withId(R.id.history_button)).perform(ViewActions.click())
     }
 
+    @Test
+    fun openHistory() {
+        onView(ViewMatchers.withId(R.id.history_window)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
 }
